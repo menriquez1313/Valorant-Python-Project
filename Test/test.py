6fe5eab19b1eee14d0735ed0d2aabe13f149a2d1
menriquez1313 <81117223+menriquez1313@users.noreply.github.com>
2022-10-14 14:32:19 -0700

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -8,17 +8,45 @@ This is a test using the Valorant API
 import requests
 import valorant
 
+
 response = requests.get("API")
+val_status = requests.get("API")
 
-response.raise_for_status()
+def user_info_status():
+    response.raise_for_status()
+    val_status.raise_for_status()
 
-data = response.json()
+    data = response.json()
+    data_val_status = val_status.json()
 
-id = data["gameName"]
-tag = data["tagLine"]
+    id = data["gameName"]
+    tag = data["tagLine"]
+    
+    location = data_val_status["name"]
+    locales = data_val_status["locales"][2]
+    valorantTag = (id, tag)
+    location_info = (location, locales)
 
-valorantTag = (id, tag)
+    print(f"User's Valorant Tag: {valorantTag}")
+    print(f"Location: {location_info}")
+    
 
-print(valorantTag)
+on = TRUE
+while on == TRUE:
+    print("/////////Welcome to VALORANT info///////")
+    print("1: User and locale")
+    print("0: Exit")
+    choose = input("What info would you like to know?: ")
+    if choose == 1:
+        user_info_status()
+        print("\n")
+    elif choose == 0:
+        print("GLHF on your Games!")
+        on == FALSE
+    else:
+        print("Please enter a number showed on the list.")
+        
+
+
 
 
