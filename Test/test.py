200453da9c50b82a334d9862b185f35ca110bd02
menriquez1313 <81117223+menriquez1313@users.noreply.github.com>
2022-10-14 16:10:08 -0700

    Update
    +Gives Users In-Game Name, Tag, and Region
    
    +Allows user to check on maintenance and updates on Valorant

--- Test/test.py
+++ Test/test.py
@@ -6,47 +6,59 @@ This is a test using the Valorant API
 """
 
 import requests
-import valorant
 
+response = requests.get("") #ACCOUNT-V1 
+val_status = requests.get("") #VAL-STATUS-v1
 
-response = requests.get("API")
-val_status = requests.get("API")
+data = response.json()
+data_val_status = val_status.json()
 
+#Gives out basic info of User
 def user_info_status():
     response.raise_for_status()
     val_status.raise_for_status()
 
-    data = response.json()
-    data_val_status = val_status.json()
-
     id = data["gameName"]
     tag = data["tagLine"]
+    #puuid = data["puuid"]
     
     location = data_val_status["name"]
     locales = data_val_status["locales"][2]
-    valorantTag = (id, tag)
+    valorantTag = (id, tag) #puuid)
     location_info = (location, locales)
 
     print(f"User's Valorant Tag: {valorantTag}")
     print(f"Location: {location_info}")
-    
 
-on = TRUE
-while on == TRUE:
+#Checks for status in specified region (NA)
+def valorant_status():
+    val_status.raise_for_status()
+    2
+    maintenances = data_val_status["maintenances"]
+    incidents = data_val_status["incidents"][0]["updates"][0]["translations"][0]["content"]
+    
+    print(f"Maintenances: {maintenances}")
+    print(f"Updates: {incidents}")
+
+on = True
+while on == True:
     print("/////////Welcome to VALORANT info///////")
     print("1: User and locale")
+    print("2: Update/Status on Valorant")
     print("0: Exit")
-    choose = input("What info would you like to know?: ")
+    choose = int(input("What info would you like to know?: "))
+    
+    
     if choose == 1:
         user_info_status()
         print("\n")
+    elif choose == 2:
+        valorant_status()
+        print("\n")
     elif choose == 0:
         print("GLHF on your Games!")
-        on == FALSE
+        on = False
+        print("\n")
     else:
         print("Please enter a number showed on the list.")
-        
-
-
-
 
